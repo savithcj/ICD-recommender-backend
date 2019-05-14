e76e7c9ebc5f6606f6be3f2806a37a05a3b0095a
airoscar <oscar.y.chen@gmail.com>
2019-05-14 16:30:18 -0600

    added a button to make API call

--- Django/website/settings.py
+++ Django/website/settings.py
@@ -43,9 +43,12 @@ INSTALLED_APPS = [
     'recommendations',
     'web',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': 
+    ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
+}
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
