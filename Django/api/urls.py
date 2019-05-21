8f7d8b05ecd1bc5edcf603c1f5424f6354831b6f
Savith Jayasekera <scjayase@ucalgary.ca>
2019-05-20 19:45:40 -0600

    modified API to get info for individual codes

--- Django/api/urls.py
+++ Django/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 urlpatterns = [
     path('rules/', views.ListAllRules.as_view(), name="rules-all"),
     path('codes/', views.ListAllCodes.as_view(), name="codes-all"),
+    path('codes/<str:pk>/', views.CodeInformation.as_view(), name="code-information"),
     path('children/<str:pk>/', views.ListChildrenOfCode.as_view(), name="children-of-code"),
 ]
--- Django/api/views.py
+++ Django/api/views.py
@@ -6,8 +6,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework import permissions
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from recommendations.models import Rule, Code
+
 
 # Create your views here.
 
@@ -26,6 +27,14 @@ class ListAllCodes(generics.ListAPIView):
 
 
 @permission_classes((permissions.AllowAny,))
+class CodeInformation(APIView):
+    def get(self, request, pk, format=None, **kwargs):
+        CodeInfo = get_object_or_404(Code, pk=pk)
+        serializer = serializers.CodesSerializer(CodeInfo)
+        return Response(serializer.data)
+
+
+@permission_classes((permissions.AllowAny,))
 class ListChildrenOfCode(APIView):
     def get_object(self, pk):
         try:
@@ -35,10 +44,10 @@ class ListChildrenOfCode(APIView):
             return children
         except ObjectDoesNotExist:
             return Code.objects.none()
-    
+
     def get(self, request, pk, format=None, **kwargs):
         children = self.get_object(pk)
         serializer = serializers.ChildrenSerializer(children, many=True)
         return Response(serializer.data)
 
-#TO DO: implement access permissions?
+# TO DO: implement access permissions?
