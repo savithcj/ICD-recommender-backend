545aef24f18580d45e749dca3e4e6bc72fca7ca5
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-06-02 00:22:18 -0600

    Fixed bug with slashes at the end of the url

--- Django/api/urls.py
+++ Django/api/urls.py
@@ -3,9 +3,9 @@ from api import views
 
 urlpatterns = [
     path('rules/', views.ListAllRules.as_view(), name="rules-all"),
-    path('children/<str:pk>', views.ListChildrenOfCode.as_view(), name="children-of-code"),
-    path('family/<str:pk>', views.Family.as_view(), name="family-of-code"),
-    path('codeDescription/<str:pk>', views.SingleCodeDescription.as_view(), name="single-code"),
-    path('requestRules/<str:inCodes>', views.ListRequestedRules.as_view(), name="rules-specific")
+    path('children/<str:pk>/', views.ListChildrenOfCode.as_view(), name="children-of-code"),
+    path('family/<str:pk>/', views.Family.as_view(), name="family-of-code"),
+    path('codeDescription/<str:pk>/', views.SingleCodeDescription.as_view(), name="single-code"),
+    path('requestRules/<str:inCodes>/', views.ListRequestedRules.as_view(), name="rules-specific")
 
 ]
