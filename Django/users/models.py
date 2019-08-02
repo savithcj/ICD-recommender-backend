7e7ef185b1c8980d11bd1cd4d4e0cb1173eb3486
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-08-02 14:12:30 -0600

    Added choices to roles

--- Django/users/models.py
+++ Django/users/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    role = models.CharField(db_column='role', max_length=50, default='coder')
+    role = models.CharField(db_column='role', max_length=50, choices=[
+                            ('coder', 'coder'), ('admin', 'admin')], default='coder')
     verified = models.BooleanField(db_column='verified', default=False)
