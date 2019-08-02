27b2e6409a4fb78a703541e95b0ad72b019fccca
Savith Jayasekera <scjayase@ucalgary.ca>
2019-08-01 18:14:54 -0600

    changed token expiry time to 10 hours

--- Django/website/settings.py
+++ Django/website/settings.py
@@ -145,7 +145,7 @@ OAUTH2_PROVIDER = {
     # this is the list of available scopes
     'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'},
     'OAUTH2_BACKEND_CLASS': 'oauth2_provider.oauth2_backends.JSONOAuthLibCore',
-    'ACCESS_TOKEN_EXPIRE_SECONDS': 10
+    'ACCESS_TOKEN_EXPIRE_SECONDS': 36000
 }
 
 
