2c19b03ee1d2c3bfebd47602729f34ca9550334e
Alexander Krusina <alexander.krusina@ucalgary.ca>
2019-08-04 15:15:28 -0600

    Adding parents recursively to check for rules

--- Django/api/views.py
+++ Django/api/views.py
@@ -213,17 +213,24 @@ class ListRequestedRules(APIView):
             inputCodes = inCodes
             inputCodes = inputCodes.split(",")
 
-            codesToAdd = []
+            codesToAdd = set()
             for code in inputCodes:
                 parent = Code.objects.get(code=code).parent
-                codesToAdd.append(parent)
-            inputCodes += codesToAdd
+                codesToAdd.add(parent)
+                while True:
+                    try:
+                        parent = Code.objects.get(code=parent).parent
+                        codesToAdd.add(parent)
+                    except ObjectDoesNotExist:
+                        break
+
+            inputCodes += list(codesToAdd)
             inputCodes.sort()
 
             # Build combinations of codes
-            # max combination in the LHS of 4 codes
+            # max combination in the LHS of 3 codes
             lhs = []
-            for i in range(min(len(inputCodes), 4)):
+            for i in range(min(len(inputCodes), 3)):
                 lhs += list(combinations(inputCodes, i+1))
 
             # Concatening items in combinations together
