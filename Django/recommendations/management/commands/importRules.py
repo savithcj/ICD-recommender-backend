a2afb676d857124c98cd076b81cd8fb372a45848
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-08-05 21:07:29 -0600

    Removed extra file import

--- Django/recommendations/management/commands/importRules.py
+++ Django/recommendations/management/commands/importRules.py
@@ -11,73 +11,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Reading rules...")
         Rule.objects.all().delete()  # Deletes all existing rules before importing
-
-        df1 = pd.read_csv("secret/three_digit_rules.csv")  # 3 digit oracle rules
-        df2 = pd.read_csv("secret/four_digit_rules.csv")  # 4 digit oracle rules
-        df = pd.concat([df1, df2])  # Concatenating to one dataframe
-        df = df.reset_index()
-
-        # Removing unnecessary columns
-        df = df.drop(columns=["Unnamed: 0", "id", "group", "role", "score_group", "counts", "voter_total", "round"])
-
-        lhs = []
-        rhs = []
-        age_min = []
-        age_max = []
-
-        for row in range(df.shape[0]):
-            # Making confidence to float
-            df.loc[row, "confidence"] = float(df.loc[row, "confidence"][:-1])/100
-            rule = df.loc[row, "rules"].split(" => ")  # Splitting rule into LHS and RHS
-            lhs.append(rule[0][1:-1])  # Adding LHS
-            rhs.append(rule[1][1:-1])  # Adding RHS
-            # Setting age bracket for >=65
-            if(df.loc[row, "age_group"] == ">=65"):
-                age_min.append(65)
-                age_max.append(150)
-            # Setting age bracket for other age groups
-            else:
-                age = df.loc[row, "age_group"].split(" to ")
-                age_min.append(float(age[0]))
-                age_max.append(float(age[1]))
-
-        df["confidence"] = df["confidence"].astype("float")
-        df["lhs"] = lhs
-        df["rhs"] = rhs
-        df["min_age"] = age_min
-        df["max_age"] = age_max
-
-        print("Saving...")
         ruleSet = set()  # Create a set of rules to prevent duplicate rules
         with transaction.atomic():  # Saves all of the rules at once
-            for row in range(df.shape[0]):
-                # Creating male version of the rule (oracle rules did not have a gender)
-                ruleM = Rule.objects.create(lhs=df.loc[row, "lhs"],
-                                            rhs=df.loc[row, "rhs"],
-                                            gender='M',
-                                            min_age=df.loc[row, "min_age"],
-                                            max_age=df.loc[row, "max_age"],
-                                            support=df.loc[row, "support"],
-                                            confidence=df.loc[row, "confidence"],
-                                            oracle=True)
-                # Adding the rule to the set
-                ruleSet.add((df.loc[row, "lhs"], df.loc[row, "rhs"], 'M',
-                             df.loc[row, "min_age"], df.loc[row, "max_age"]))
-                ruleM.save()  # Saving
-
-                # Creating female version of the rule
-                ruleF = Rule.objects.create(lhs=df.loc[row, "lhs"],
-                                            rhs=df.loc[row, "rhs"],
-                                            gender='F',
-                                            min_age=df.loc[row, "min_age"],
-                                            max_age=df.loc[row, "max_age"],
-                                            support=df.loc[row, "support"],
-                                            confidence=df.loc[row, "confidence"],
-                                            oracle=True)
-                # Adding the rule to the set
-                ruleSet.add((df.loc[row, "lhs"], df.loc[row, "rhs"], 'F',
-                             df.loc[row, "min_age"], df.loc[row, "max_age"]))
-                ruleF.save()
             # Adding the additional mined rules
             with open("secret/output_rules_cleaned.csv", 'r') as f:
                 for i, line in enumerate(f.readlines()):
