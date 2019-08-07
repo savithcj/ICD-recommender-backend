aaa53482bc79672c3eefd17251c99b8251046af2
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-08-07 10:26:32 -0600

    Changed database settings

--- Django/recommendations/management/commands/createsu.py
+++ Django/recommendations/management/commands/createsu.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from users.models import CustomUser
 import os
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", os.environ['DJANGO_ADMIN_PASSWORD'])
+        if not CustomUser.objects.filter(username="admin").exists():
+            CustomUser.objects.create_superuser("admin", "admin@admin.com", os.environ['DJANGO_ADMIN_PASSWORD'])
