8063158188b7016b2173c105728e474cc75899b9
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-08-16 14:22:02 -0600

    Removed old code

--- Django/recommendations/urls.py
+++ Django/recommendations/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('entering/', views.entering.as_view(), name='entering'),
-    path('results/', views.results.as_view(), name='results'),
 ]
